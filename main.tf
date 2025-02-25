109c4e3a90b0d1e4873b4eb253869d65e6c41e72
Shyamprasad <admin@Admins-MacBook-Pro.local>
2025-02-25 20:34:00 +0530

    Adding main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+  region "us-west-2"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+
